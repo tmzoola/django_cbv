d84b082948d20a373a04ccd2a37dcb56ece5afc6
Murodjon Tokhirov <murodtokh1997@gmail.com>
2023-03-01 21:07:13 +0900

    Forms added

--- classroom/urls.py
+++ classroom/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomeView,ThankYouView
+from .views import HomeView,ThankYouView,ContactFormView
 
 app_name = 'classroom'
 
 urlpatterns = [
     path('home_view/',HomeView.as_view(), name='home_view'),
     path('thank_you/',ThankYouView.as_view(), name='thank_you'),
+    path('contact/',ContactFormView.as_view(), name='contact'),
 ]
--- classroom/views.py
+++ classroom/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from django.views.generic import TemplateView
-
+from django.views.generic import TemplateView,FormView
+from .forms import ContactForm
 
 
 
@@ -11,3 +11,14 @@ class HomeView(TemplateView):
 
 class ThankYouView(TemplateView):
     template_name = 'classroom/thank_you.html'
+
+
+class ContactFormView(FormView):
+    form_class = ContactForm
+    template_name = 'classroom/contact.html'
+
+    success_url = "/classroom/thank_you"
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
